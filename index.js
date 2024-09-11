d1fa5a31dbad179827018219cfa9482cc35ab1bd
juhllian <juhlliangmz@gmail.com>
2024-09-11 18:34:43 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -59,8 +59,25 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas" + realizadas
+        .length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async  () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length ==0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -72,6 +89,7 @@ const start = async () => {
                 { name: "Cadastrar Meta", value: "cadastrar" },
                 { name: "Listar Metas", value: "listar" },
                 { name: "Metas realizadas", value: "realizadas" },
+                { name: "Metas abertas", value: "abertas" },
                 { name: "Sair", value: "sair" }
             ]
         });
@@ -86,6 +104,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas() 
+                break
+            case "abertas":
+                await metasAbertas() 
                 break
             case "sair":
                 console.log("Até a próxima!");
